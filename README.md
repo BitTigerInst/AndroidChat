4ebc2c158334934fa7d06bee7a527dc08a806072
antonylhz <antony26@cs.rutgers.edu>
2016-02-15 00:17:38 -0500

    0.1.1

--- README.md
+++ README.md
@@ -31,4 +31,3 @@ Owner
 -----
 
 @team: MonkeyBOOM
-
---
+++ app/src/main/java/com/firebase/androidchat/util/Validator.java
@@ -0,0 +1,48 @@
+package com.firebase.androidchat.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by antonylhz on 2/14/16.
+ * <p/>
+ * Validation related functions and exception classes
+ */
+public class Validator {
+
+    private static final int PASSWORD_MIN_LENGTH = 4;
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(
+            "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+            Pattern.CASE_INSENSITIVE
+    );
+
+    public void checkEmail(String email) {
+        if (null == email || email.length() < 1) {
+            throw new EmptyEmailException();
+        }
+        Matcher matcher = EMAIL_PATTERN.matcher(email);
+        if (!matcher.find()) {
+            throw new InvalidEmailException();
+        }
+    }
+
+    public void checkPassword(String password) {
+        if (password == null ||
+                password.length() < PASSWORD_MIN_LENGTH) {
+            throw new ShortPasswordException();
+        }
+    }
+
+    public class EmptyEmailException extends RuntimeException {
+
+    }
+
+    public class InvalidEmailException extends RuntimeException {
+
+    }
+
+    public class ShortPasswordException extends RuntimeException {
+
+    }
+
+}
